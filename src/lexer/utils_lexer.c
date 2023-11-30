b509cbc2492af41f07aeb402173247afbb523c34
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-11-30 16:12:35 +0100

    double print_error - restructurized

--- src/lexer/utils_lexer.c
+++ src/lexer/utils_lexer.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/18 19:15:55 by fras          #+#    #+#                 */
-/*   Updated: 2023/11/30 15:35:22 by fras          ########   odam.nl         */
+/*   Updated: 2023/11/30 16:11:39 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,21 @@
 void	error_output(char *string)
 {
 	write(STDERR_FILENO, string, ft_strlen(string));
+}
+
+void	print_error(t_error_type error)
+{
+	if (error == MALLOC_FAILED)
+		error_output("MINISHELL ERROR: malloc failed.\n");
+	if (error == UNCLOSED_QUOTE)
+		error_output("MINISHELL ERROR: unclosed quotation detected.\n");
+	if (error == INVALID_SPECIAL_CASE)
+		error_output("MINISHELL ERROR: syntax error. " \
+					"Check special characters.\n");
+	if (error == PIPE_UNLOGICAL)
+		error_output("MINISHELL ERROR: syntax error. " \
+					"Expected command, received pipe.\n");
+	if (error == FILENAME_MISSING)
+		error_output("MINISHELL ERROR: syntax error. " \
+					"Filename not defined for redirection.\n");
 }
